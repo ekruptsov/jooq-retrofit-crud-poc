e08789b072d56e450483fb3bcdaad39cfe30ac85
ekruptsov <eugene.krupsov@gmail.com>
2023-08-01 09:27:50 +0200

    Update gradle wrapper

--- build.gradle.kts
+++ build.gradle.kts
@@ -90,7 +90,7 @@ dependencyManagement {
 val postgresqlSQLContainer = tasks.create("postgresqlContainer") {
     if (project.gradle.startParameter.taskNames.any { it.contains("flyway|Jooq".toRegex()) }) {
         @Suppress("UPPER_BOUND_VIOLATED_WARNING")
-        val instance = PostgreSQLContainer<PostgreSQLContainer<Nothing>>("postgres:latest")
+        val instance = PostgreSQLContainer("postgres:latest")
             .withDatabaseName("poc_crud").apply { start() }
         extra.apply {
             set("jdbc_url", instance.jdbcUrl)
